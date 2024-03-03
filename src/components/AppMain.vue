ea098ccdfeb922d6a5e9bb27d70256710f5804f7
igorgaidolfi <gandalf.4@outlook.it>
2024-03-03 22:54:51 +0100

    Milestone 2,3,4 done

---
+++ src/components/AppMain.vue
@@ -0,0 +1,38 @@
+<script>
+import {store} from '../store.js' 
+import axios from 'axios'
+
+export default {
+    name: 'AppMain',
+    data(){
+        return{
+            store,
+            projects:[]
+        }
+    },
+    created(){
+        this.getProjects()
+    },
+    methods:{
+        getProjects(){
+            axios.get(`${this.store.apiUrl}/api/projects`).then((response) =>{
+                console.log(response.data.results)
+            })
+        }
+    }
+}
+</script>
+<template lang="">
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+<style lang="">
+    
+</style>
